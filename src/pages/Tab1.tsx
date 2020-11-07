876d59d982391ef18e05297ef383936410aa67f0
gotjosh <gitlab@begreen.nu>
2020-11-07 11:11:51 +0000

    closes #2 render mockdata as simple list

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import {mockresponse} from '../data/issues'
 
 const Tab1: React.FC = () => {
+  
+  let issues = mockresponse.data.repository.issues.edges.map(eachEdge => eachEdge.node)
+  // console.log(issues)
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Issues</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Github Issue List</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          {issues.map(eachIssue => (
+            <IonItem>
+              <IonLabel>{eachIssue.title}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
